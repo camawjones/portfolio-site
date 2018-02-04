fbca331eae3835cb5356b1e256e8915fc8425bd3
camawjones <camawjones@gmail.com>
2018-02-04 21:42:42 +0000

    cname added

--- src/components/page4.js
+++ src/components/page4.js
@@ -4,41 +4,46 @@ import VisibilitySensor from 'react-visibility-sensor';
 class Page4 extends React.Component {
   state = {
     visible: false
-  }
+  };
 
-  onChange = (isVisible) => {
-    isVisible ? this.setState({
-      visible: true
-    })
+  onChange = isVisible => {
+    isVisible
+      ? this.setState({
+          visible: true
+        })
       : this.setState({
-        visible: false
-      });
-    console.log(this.state.visible);
-  }
+          visible: false
+        });
+  };
 
   render() {
     const componentClasses = ['shy'];
     const componentClassesMail = ['email'];
-    if(this.state.visible) {
+    if (this.state.visible) {
       componentClasses.push('show');
       componentClassesMail.push('show');
     }
 
-
-    return(
+    return (
       <section className="flex align page4">
         <div className="mail">
           <VisibilitySensor onChange={this.onChange} />
-          <p className={componentClasses.join(' ')}>{"Don't be shy, say hi!"}</p>
+          <p className={componentClasses.join(' ')}>
+            {"Don't be shy, say hi!"}
+          </p>
           <div className={componentClassesMail.join(' ')}>
-            <div><i className="fa fa-envelope" aria-hidden="true"></i> camawjones@gmail.com</div>
-            <p><i className="fa fa-phone" aria-hidden="true"></i> +447884063956</p>
+            <div>
+              <i className="fa fa-envelope" aria-hidden="true" />{' '}
+              camawjones@gmail.com
+            </div>
+            <p>
+              <i className="fa fa-phone" aria-hidden="true" /> +447884063956
+            </p>
           </div>
         </div>
       </section>
     );
   }
-
 }
 
 export default Page4;
